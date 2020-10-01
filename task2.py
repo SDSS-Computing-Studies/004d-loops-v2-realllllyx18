7aa90e19e74f022edb47e889bed9b918f9bcd145
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-01 12:35:59 -0700

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,17 @@
+#! python
+
+name=input("Enter a name")
+players = ("Guile", "Cammy", "Ryu", "Ken", "Chun-Li")
+
+for x in players:
+    print("Match")
+    if x=="Guile":
+        break
+    if x=="Cammy":
+        break
+    if x=="Ryu":
+        break
+    if x=="Ken":
+        break
+    if x=="Chun-Li":
+        break
