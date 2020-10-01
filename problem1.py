aa8d5c6b5b605e54acb9b86648d3ac90ff276353
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-01 12:58:58 -0700

    Update problem1.py

---
+++ problem1.py
@@ -0,0 +1,10 @@
+#! python
+
+integer1=input("Enter the width")
+integer2=input("Enter the length")
+w=int(integer1)
+h=int(integer2)
+
+for w in range (1,11):
+   for h in range(1,11):
+       result=
