21f8bafe49816f474c6eed48c3af9bdcb354419a
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-02 11:31:17 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,9 +2,10 @@
 
 name=input("Enter a name")
 nameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
+
 for x in nameList:
     if x==name:
         print("That name is on the list")
         break
 if x!=name:
-        print("That name is not in the list")
+        print("That name is not on the list")
