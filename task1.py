016b9483c96f5a0ad670aaa0953eeac5876c0d2c
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-02 11:06:40 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 #! python
 
-integer=input("Enter an integer")
+integer=(input("Enter an integer")).strip()
 
 for i in range(1,13):
     z=float(integer)*i
-    print(integer,"*",i,"=",z)
+    print(z)
